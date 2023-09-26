0c42d9e82055f6badd5d9dda1206d10f2c2a9fcf
yolokazi-cloud <yolokazinogantsho@gmail.com>
2023-09-26 08:53:58 +0200

    finished product

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import  ReactDOM  from "react-dom"  
+import './style.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//user profile component
+export default function UserProfileCard(){
+    return(
+        <div className="card">
+            <img className="img" src="../profilepic.png" alt="ProfilePicture" />
+            <div className="cardtexts">
+            <h2>Yolokazi Mmutsi</h2>
+            <h3>Software Engineer</h3>
+            <p>Passionate about developing systems. I am proficient in frontend and backend development with React, Java, C#.</p> 
+            <p id="statss">100 Followers |  100  Following  |  34 Projects </p>
+        
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            <div className="buttons">
+                <button>Follow</button>
+                <button>View Profile</button>
+            </div>
+            </div>
+            
+        </div>
+    )
+}
+const App = (
+    <div>
+        <UserProfileCard/>
+    </div>
+)
+ ReactDOM.render(App , document.getElementById("root"))
